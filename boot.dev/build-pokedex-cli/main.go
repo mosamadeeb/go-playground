72c1b9ad3f20b679bf9de4b19848f18564adcfc0
mosamadeeb <mosamaeldeeb@gmail.com>
2024-08-31 09:15:27 +0300

    [pokedexcli] 3: REPL refactor (according to hints)

--- boot.dev/build-pokedex-cli/main.go
+++ boot.dev/build-pokedex-cli/main.go
@@ -6,28 +6,62 @@ import (
 	"os"
 )
 
+type command struct {
+	name     string
+	usage    string
+	callback func() error
+}
+
+// This is used to determine the order of enumeration of the commands
+var commandList = []string{
+	"help", "exit",
+}
+
+// An initializer is not used to avoid a circular dependency with the [commandHelp] function
+var commandMap map[string]command = make(map[string]command)
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, com := range commandList {
+		fmt.Printf("%s: %s\n", com, commandMap[com].usage)
+	}
+
+	return nil
+}
+
+func commandExit() error {
+	// Technically, a defer here makes no sense because the return value won't reach the caller anyway
+	defer os.Exit(0)
+	return nil
+}
+
+func initCommandMap() {
+	commandMap["help"] = command{"help", "Displays a help message", commandHelp}
+	commandMap["exit"] = command{"exit", "Exit the Pokedex", commandExit}
+}
+
 func main() {
+	initCommandMap()
+
 	scanner := bufio.NewScanner(os.Stdin)
 
-Repl:
 	for {
-		fmt.Print("pokedex > ")
+		fmt.Print("Pokedex > ")
 
 		// We're scanning Stdin so no need to check if there are no tokens
 		scanner.Scan()
 		commandText := scanner.Text()
 
-		switch commandText {
-		case "help":
-			fmt.Println("Pokedex CLI")
-			fmt.Println("usage:")
-			fmt.Println("	help	Show this help message.")
-			fmt.Println("	exit	Exit the program.")
-		case "exit":
-			break Repl
-		default:
+		com, ok := commandMap[commandText]
+		if ok {
+			com.callback()
+		} else {
 			fmt.Println("Unexpected command: ", commandText)
-			fmt.Println("Use the command \"help\" to see usage.")
+			fmt.Println("Use the command \"help\" to see usage")
 		}
 
 		fmt.Println()
