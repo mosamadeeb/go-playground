ad35169025da3251b55b253db0958775fde7c116
mosamadeeb <mosamaeldeeb@gmail.com>
2024-08-31 08:50:25 +0300

    [pokedexcli] 3: REPL

--- boot.dev/build-pokedex-cli/main.go
+++ boot.dev/build-pokedex-cli/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+Repl:
+	for {
+		fmt.Print("pokedex > ")
+
+		// We're scanning Stdin so no need to check if there are no tokens
+		scanner.Scan()
+		commandText := scanner.Text()
+
+		switch commandText {
+		case "help":
+			fmt.Println("Pokedex CLI")
+			fmt.Println("usage:")
+			fmt.Println("	help	Show this help message.")
+			fmt.Println("	exit	Exit the program.")
+		case "exit":
+			break Repl
+		default:
+			fmt.Println("Unexpected command: ", commandText)
+			fmt.Println("Use the command \"help\" to see usage.")
+		}
+
+		fmt.Println()
+	}
 }
