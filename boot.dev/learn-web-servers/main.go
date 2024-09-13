c27b8f2a80913457b69b72f44f823b23ae8ab479
mosamadeeb <mosamaeldeeb@gmail.com>
2024-09-13 09:20:09 +0300

    [chirpy] Add endpoints for checking and resetting metrics
    Learn Web Servers | Ch 2: Routing | 1: Stateful Handlers

--- boot.dev/learn-web-servers/main.go
+++ boot.dev/learn-web-servers/main.go
@@ -5,7 +5,35 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+// A simple middleware that inserts a handler in between
+// This allows us to do something before the next handler is used
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+// We can return a handler by wrapping a function with an appropriate signature with http.HandlerFunc()
+func (c *apiConfig) metricsHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Hits: %v", c.fileserverHits)))
+	})
+}
+
+func (c *apiConfig) resetHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits = 0
+	})
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
 	serve := http.Server{
 		Handler: mux,
@@ -13,7 +41,11 @@ func main() {
 	}
 
 	// Serve files in local directory without the /app prefix
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+
+	mux.Handle("/metrics", apiCfg.metricsHandler())
+	mux.Handle("/reset", apiCfg.resetHandler())
 
 	// Readiness endpoint
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
