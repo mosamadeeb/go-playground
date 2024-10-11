7b7395fdf494483a279e3cf210922835a4867e41
mosamadeeb <mosamaeldeeb@gmail.com>
2024-10-11 21:37:36 +0300

    [chirpy] Fix user schema id type not being UUID

--- boot.dev/learn-http-servers/sql/schema/001_users.sql
+++ boot.dev/learn-http-servers/sql/schema/001_users.sql
@@ -1,7 +1,7 @@
 -- Active: 1728656163713@@127.0.0.1@5432@chirpy
 -- +goose Up
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     email TEXT NOT NULL UNIQUE
